9c63323f493ced19590ea6ad759670bd45f6e2d5
oveljosland <oveljosland@icloud.com>
2025-10-30 20:53:20 +0100

    rename for clarity

--- src/pkg.vhd
+++ src/pkg.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 package pkg is
 	constant BITWIDTH: positive := 8;
 	constant BAUDRATE: positive := 9600; /* B/s */
-	constant NSAMP: positive := 8; /* oversampling */
+	constant SMP_PER_BIT: positive := 8; /* oversamples per bit */
 	constant SYS_CLK_FRQ: positive := 50; /* MHz */
 	constant CLK_PER_BIT: positive := SYS_CLK_FRQ * 1000000 / BAUDRATE;
 	constant CLK_PER_SMP: positive := CLK_PER_BIT / NSAMP; /* cycles/sample */
