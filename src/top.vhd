cada6f169b87d35f2b89fb48175320f6f28e3345
oveljosland <oveljosland@icloud.com>
2025-10-30 19:18:29 +0100

    uncomment port map temporarily, working and compiling in quartus

--- src/top.vhd
+++ src/top.vhd
@@ -11,7 +11,7 @@ entity top is
 		rstn: in std_logic; /* active low */
 
 		rx: in std_logic;
-		tx; out std_logic;
+		tx: out std_logic;
 
 		valid: out std_logic; /* rx data valid */
 		busy: out std_logic /* tx busy */
@@ -20,13 +20,15 @@ end entity;
 
 architecture rtl of top is
 begin
+	/*
 	tx_module: entity work.utx
 	port map (
-		/* TODO: map signals */
+		-- TODO: map signals
 	);
 
 	rx_module: entity work.urx
 	port map (
-		/* TODO: map signals */
+		-- TODO: map signals
 	);
+	*/
 end architecture;
