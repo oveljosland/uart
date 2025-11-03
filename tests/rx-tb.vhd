ba0daed0fc9a66c87e87f4c4f579f5053ab81d4b
alistermr <ali.amlashii@gmail.com>
2025-11-03 13:26:39 +0100

    enda en test

--- tests/rx-tb.vhd
+++ tests/rx-tb.vhd
@@ -1,3 +1,19 @@
 /* testbench for rx module */
 /* Denne testbenken skal kunne simulere oppførselen til rx modulen */
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+library work;
+use work.pkg.all; -- BITWIDT, SMP_PER_BIT, CLK_PER_SMP
+
+entity rx_tb is
+end entity;
+
+architecture simulation of rx_tb is 
+    constant CLK_PERIOD : time := 20 ns; -- Klokkeperiode (20ns = 50 Mhz)
+    constant BIT_CYCLES : natural := SMP_PER_BIT * CLK_PER_SMP -- Antall klokkesykluser per UART-bit -> 8 samples * antall klokker per sample
+
+    
