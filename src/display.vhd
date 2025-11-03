1e90ba7f472800de56387bf699705355cac027bb
oveljosland <oveljosland@icloud.com>
2025-11-03 21:31:39 +0100

    add ports and putchar proc

--- src/display.vhd
+++ src/display.vhd
@@ -1,8 +1,22 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.pkg.all;
 
 entity display is
+	port (
+		char: in std_logic_vector(BITWIDTH - 1 downto 0);
+		seg: out std_logic_vector(BITWIDTH - 1 downto 0)
+	);
 end entity;
 
 architecture rtl of display is
+begin
+	/* putchar:  put character onto the display */
+	put_char: process(char)
+		variable a: integer;
+	begin
+		a := to_integer(char);
 end architecture;
