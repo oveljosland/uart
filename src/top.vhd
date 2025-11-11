c5c9c4772e9b8d9a64233faf046a0a0269090f09
oveljosland <oveljosland@icloud.com>
2025-11-11 14:57:08 +0100

    replace control module with top module

--- src/top.vhd
+++ src/top.vhd
@@ -5,6 +5,17 @@ use ieee.numeric_std.all;
 library work;
 use work.pkg.all;
 
+/* control/top module */
+
+/* requirements */
+-- print ascii code to seven-segment display.
+-- flash led when a character is received.
+-- loopback: send received character back immediately.
+-- should send predetermined character with a button press.
+-- can send a predetermined string of at least eight characters.
+-- can adjust baud rate while running (100 kb/s – 1 Mb/s in 100 kb/s steps).
+
+
 entity top is
 	port ( /* TODO: decide which ports to consider */
 		clk: in std_logic; /* SYS_CLK_FRQ defined in pkg */
