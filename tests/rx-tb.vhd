73d87815bc306de6f3c9a63f3dcc27ae18a11063
YoPotato80 <mathias.schiotz@hotmail.no>
2025-11-16 18:31:11 +0100

    added constants used in tb

--- tests/rx-tb.vhd
+++ tests/rx-tb.vhd
@@ -9,6 +9,9 @@ entity rx_tb is
 end entity;
 
 architecture simulation of rx_tb is 
+    constant CLK_PER_BIT: positive := SYS_CLK_FRQ * 1_000_000 / BAUDRATE;
+	constant CLK_PER_SMP: positive := CLK_PER_BIT / SMP_PER_BIT;
+
     constant CLK_PERIOD : time := 20 ns; -- Klokkeperiode (20ns = 50 Mhz)
     constant BIT_CYCLES : natural := SMP_PER_BIT * CLK_PER_SMP; -- Antall klokkesykluser per UART-bit -> 8 samples * antall klokker per sample
 
